8661002768bdf6ea324609e3e448fdeaee861d0a
AdemirRamos <ademirramos634@gmail.com>
2022-06-30 00:12:07 -0300

    Adição da Aula 03
    Neste 'commit' foram adicionados os arquivos e pastas que compõem a aula 03 (JSX explicação) do curso de React.

---
+++ Matheus Battisti - Hora de Codar/Aula 03/aula03.js
@@ -0,0 +1,15 @@
+/*
+
+    Entendendo o JSX
+
+    * O JSX é como um HTML, porém dentro do código Javascript;
+
+    * É a principal maneira de escrever HTML com o React;
+
+    * Podemos interpolar variáveis, inserindo elas entre "{}" (chaves);
+
+    * É possível, também, executar funções em JSX;
+
+    * Inserir valores em atributos de "tags" também é valido em JSX.
+
+*/
