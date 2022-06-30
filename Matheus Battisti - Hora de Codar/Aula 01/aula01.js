0d4101e7906aa606cc077a7c250d4e0740020525
AdemirRamos <ademirramos634@gmail.com>
2022-06-29 23:20:19 -0300

    Adição da Aula 01
    Neste 'commit' foi adicionado o arquivo 'aula01.js'.

---
+++ Matheus Battisti - Hora de Codar/Aula 01/aula01.js
@@ -0,0 +1,17 @@
+/*
+
+    Introdução - O que é o React?
+
+    * Uma biblioteca JS para a criação de interfaces;
+
+    * Utilizado para construir SPAs ("Single Page Application");
+
+    * Baseado em componentes;
+
+    * Utiliza o JSX para renderizar HTML;
+
+    * E aplica o "Virtual DOM" para realizar as alterações no DOM;
+
+    * Podemos adicionar a um projeto ou criar um projeto com ele.
+
+*/
