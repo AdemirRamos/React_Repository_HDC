b3da95052a7a88562630097d336abb6e622dbf5d
AdemirRamos <ademirramos634@gmail.com>
2022-06-29 23:37:11 -0300

    Adição da Aula 02
    Neste 'commit' foi adicionado o arquivo 'aula02.js'.

---
+++ Matheus Battisti - Hora de Codar/Aula 02/aula02.js
@@ -0,0 +1,15 @@
+/*
+
+    Como Instalar o React?
+
+    * Para instalar o React vamos utilizar uma ferramenta chamada "Create React App";
+
+    * Recebemos todos os arquivos da biblioteca e temos como executá-la;
+
+    * Para utilizá-la, precisamos do Node e também do npm;
+
+    * Essa ferramenta também otimiza o app gerado para produção;
+    
+    * É possível iniciar a aplicação com "npm start".
+
+*/
